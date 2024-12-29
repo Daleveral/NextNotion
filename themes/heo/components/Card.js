9c8e9817dd92f30564743f0f9a28cd5ea30b4114
DaleChu <ringingtrue7@gmail.com>
2024-12-29 17:24:24 +0800

    remove hexo

--- themes/heo/components/Card.js
+++ themes/heo/components/Card.js
@@ -7,3 +7,15 @@ const Card = ({ children, headerSlot, className }) => {
   </div>
 }
 export default Card
+
+// hexo card.js : 
+
+// const Card = ({ children, headerSlot, className }) => {
+//   return <div className={className}>
+//     <>{headerSlot}</>
+//     <section className="card shadow-md hover:shadow-md dark:text-gray-300 border dark:border-black rounded-xl lg:p-6 p-4 bg-white dark:bg-hexo-black-gray lg:duration-100">
+//         {children}
+//     </section>
+//   </div>
+// }
+// export default Card
