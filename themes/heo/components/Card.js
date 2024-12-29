eae6d2bb44b56aac89d685db1a9c15180a4b6f9b
DaleChu <ringingtrue7@gmail.com>
2024-12-29 17:31:18 +0800

    no hexo it's heo

--- themes/heo/components/Card.js
+++ themes/heo/components/Card.js
@@ -1,21 +1,9 @@
 const Card = ({ children, headerSlot, className }) => {
-  return <div className={`${className || ''} card border dark:border-gray-700 rounded-xl lg:p-6 p-4`}>
+  return <div className={className}>
     <>{headerSlot}</>
-    <section>
+    <section className="card shadow-md hover:shadow-md dark:text-gray-300 border dark:border-black rounded-xl lg:p-6 p-4 bg-white dark:bg-hexo-black-gray lg:duration-100">
         {children}
     </section>
   </div>
 }
 export default Card
-
-// hexo card.js : 
-
-// const Card = ({ children, headerSlot, className }) => {
-//   return <div className={className}>
-//     <>{headerSlot}</>
-//     <section className="card shadow-md hover:shadow-md dark:text-gray-300 border dark:border-black rounded-xl lg:p-6 p-4 bg-white dark:bg-hexo-black-gray lg:duration-100">
-//         {children}
-//     </section>
-//   </div>
-// }
-// export default Card
